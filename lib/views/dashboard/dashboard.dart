41f971f263b028c63471e9b0f9abf6190c48e4e0
Dhruv Tiwari <dhruv.d.tiwari@gmail.com>
2024-03-11 15:33:11 +0530

    dashboard

--- lib/views/dashboard/dashboard.dart
+++ lib/views/dashboard/dashboard.dart
@@ -3,43 +3,119 @@ import 'package:blinderville/views/dashboard/profile.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
-class Dashboard extends ConsumerWidget {
+class Dashboard extends ConsumerStatefulWidget {
   Dashboard({super.key});
-  int pageIndex = 0;
-  Map pages = {
-    'home': Icons.home_rounded,
-    'profile': Icons.person_2_rounded,
-    'forums': Icons.group_rounded,
-    'events': Icons.event_rounded,
-    'notifications': Icons.notifications_rounded
-  };
+
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  ConsumerState<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends ConsumerState<Dashboard> {
+  int pageIndex = 0;
+
+  List pages = [[
+    'home', Icons.home_rounded],
+    ['profile', Icons.person_2_rounded],
+    ['forums', Icons.group_rounded],
+    ['events', Icons.event_rounded],
+    ['notifications', Icons.notifications_rounded]
+  ];
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+        Container(
+      width: double.infinity,
+      height: 80,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Container(
-            width: double.infinity,
-            height: 80,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Text('blinderville'),
-                ListView(
-                                  shrinkWrap: true,
-                                  scrollDirection: Axis.horizontal,
-                                  children: pages.entries.map((e) => Padding(
-                  padding: const EdgeInsets.fromLTRB(10,5,10,0),
-                  child: IconButton(onPressed: () {}, icon: Icon(e.value),iconSize: 40,padding: EdgeInsets.fromLTRB(50, 0, 50,0),),
-                )).toList(),
-                                )
-              ],
-            ),
-          ),
-          // Pages
-          [Home(),Profile()][pageIndex],
+          Text('blinderville'),
+          ListView(
+                            shrinkWrap: true,
+                            scrollDirection: Axis.horizontal,
+                            children: pages.asMap().entries.map<Widget>((e) => Padding(
+            padding: const EdgeInsets.fromLTRB(10,5,10,0),
+            child: IconButton(onPressed: () {setState(() {
+              pageIndex = e.key;
+            });}, icon: Icon(e.value[1]),iconSize: 40,padding: EdgeInsets.fromLTRB(50, 0, 50,0),),
+          )).toList(),
+                          )
         ],
+      ),
+    ),
+        // Pages
+            
+                Row(
+                  children: [
+                    Flexible(
+                      
+                      flex: 1,
+                      child: ListView(physics: NeverScrollableScrollPhysics(),shrinkWrap: true, children: [
+                        SizedBox(
+                            height: 250,
+                            width: 200,
+                            child: Card(
+                              child: Column(
+                                children: [
+                                  Text('Johnson Roy'),
+                                  ElevatedButton(
+                                      onPressed: () {}, child: Text('Update ok'))
+                                ],
+                              ),
+                            )),
+                        SizedBox(
+                            height: 250,
+                            width: 200,
+                            child: Card(
+                              child: Column(
+                                children: [
+                                  Text('My follows, likes, matches'),
+                                  ElevatedButton(
+                                      onPressed: () {}, child: Text('Update ok'))
+                                ],
+                              ),
+                            )),
+                        SizedBox(
+                            height: 250,
+                            width: 200,
+                            child: Card(
+                              child: Column(
+                                children: [
+                                  Text('My purchases'),
+                                  ElevatedButton(
+                                      onPressed: () {}, child: Text('Update ok'))
+                                ],
+                              ),
+                            )),
+                        SizedBox(
+                            height: 250,
+                            width: 200,
+                            child: Card(
+                              child: Column(
+                                children: [
+                                  Text('Highlights'),
+                                  ElevatedButton(
+                                      onPressed: () {}, child: Text('Update ok'))
+                                ],
+                              ),
+                            ))
+                      ]),
+                    ),
+                    Flexible(child: [Home(), Profile()][pageIndex],
+                flex: 3,)
+                    
+                  ],
+                ),
+              
+            
+          ],
+        ),
       ),
     );
   }
 }
+
--- lib/views/dashboard/home.dart
+++ lib/views/dashboard/home.dart
@@ -1,33 +1,31 @@
 import "package:flutter/material.dart";
 
 Widget Home() {
-    return Center(
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: <Widget>[
-            const ListTile(
-              leading: Icon(Icons.person),
-              title: Text('Question of the day'),
-              subtitle: Text('When is your birthday?'),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: <Widget>[
-                TextButton(
-                  child: const Text('BUY TICKETS'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-                TextButton(
-                  child: const Text('LISTEN'),
-                  onPressed: () {/* ... */},
-                ),
-                const SizedBox(width: 8),
-              ],
-            ),
-          ],
-        ),
+    return Card(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          const ListTile(
+            leading: Icon(Icons.person),
+            title: Text('Question of the day'),
+            subtitle: Text('When is your birthday?'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              TextButton(
+                child: const Text('BUY TICKETS'),
+                onPressed: () {/* ... */},
+              ),
+              const SizedBox(width: 8),
+              TextButton(
+                child: const Text('LISTEN'),
+                onPressed: () {/* ... */},
+              ),
+              const SizedBox(width: 8),
+            ],
+          ),
+        ],
       ),
     );
 }
