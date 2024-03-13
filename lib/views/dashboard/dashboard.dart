a6cae7b7a1f9ddd19000279e9659a593ef7f11c4
Tushar Jain <tusharlock10@gmail.com>
2024-03-13 19:42:07 +0530

    wip homepage

--- lib/views/dashboard/dashboard.dart
+++ lib/views/dashboard/dashboard.dart
@@ -1,8 +1,8 @@
+import 'package:blinderville/views/dashboard/events.dart';
 import 'package:blinderville/views/dashboard/forums.dart';
 import 'package:blinderville/views/dashboard/home.dart';
 import 'package:blinderville/views/dashboard/profile.dart';
-import 'package:blinderville/views/thread.dart';
-import 'package:blinderville/views/dashboard/events.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -15,11 +15,12 @@ class Dashboard extends ConsumerStatefulWidget {
 
 class _DashboardState extends ConsumerState<Dashboard> {
   int pageIndex = 0;
+  int? _selectedTabContent = 0;
 
   List pages = [
     ['home', Icons.home_rounded],
     ['profile', Icons.person_2_rounded],
-    ['forums', Icons.group_rounded],
+    ['forums', Icons.arrow_outward_rounded],
     ['events', Icons.event_rounded],
   ];
 
@@ -35,9 +36,9 @@ class _DashboardState extends ConsumerState<Dashboard> {
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Text(
-                    'Blinderville',
-                    textScaler: TextScaler.linear(2),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Image(image: AssetImage('assets/images/logo.png')),
                   ),
                   ListView(
                     shrinkWrap: true,
@@ -54,7 +55,7 @@ class _DashboardState extends ConsumerState<Dashboard> {
                                   });
                                 },
                                 icon: Icon(e.value[1]),
-                                iconSize: 40,
+                                iconSize: 30,
                                 padding: EdgeInsets.fromLTRB(50, 0, 50, 0),
                               ),
                             ))
@@ -65,6 +66,8 @@ class _DashboardState extends ConsumerState<Dashboard> {
               ),
             ),
             // Pages
+            Divider(indent: 50, endIndent: 50),
+            SizedBox(height: 15),
 
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -73,28 +76,98 @@ class _DashboardState extends ConsumerState<Dashboard> {
                 Flexible(
                   child: Column(children: [
                     SizedBox(
-                        height: 250,
+                        height: 300,
                         width: 200,
                         child: Card(
                           child: Column(
                             children: [
-                              const Text('Johnson Roy'),
+                              SizedBox(
+                                height: 20,
+                              ),
+                              CircleAvatar(
+                                maxRadius: 40,
+                                backgroundImage: AssetImage(
+                                    'assets/images/profilePicture.jpg'),
+                              ),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              const Text(
+                                'Johnson Roy',
+                              ),
+                              SizedBox(height: 10),
+                              SizedBox(
+                                height: 100,
+                                width: 160,
+                                child: TextField(
+                                  style: TextStyle(fontSize: 12),
+                                  maxLines: 3,
+                                  decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                      borderRadius: BorderRadius.circular(10.0),
+                                    ),
+                                    filled: true,
+                                    hintStyle: TextStyle(fontSize: 12),
+                                    hintText: "About me..",
+                                  ),
+                                ),
+                              ),
+                              SizedBox(height: 10),
                               FilledButton(
-                                  onPressed: () {},
-                                  child: const Text('Update ok'))
+                                  onPressed: () {}, child: const Text('Update'))
                             ],
                           ),
                         )),
                     SizedBox(
-                        height: 250,
+                        height: 340,
                         width: 200,
                         child: Card(
                           child: Column(
                             children: [
-                              const Text('My follows, likes, matches'),
-                              FilledButton(
-                                  onPressed: () {},
-                                  child: const Text('Update ok'))
+                              Padding(
+                                padding:
+                                    const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                                child: CupertinoSlidingSegmentedControl(
+                                  backgroundColor:
+                                      Color.fromARGB(255, 28, 27, 30),
+                                  thumbColor: Color.fromARGB(255, 120, 67, 255),
+                                  children: {
+                                    0: Icon(Icons.favorite, size: 20),
+                                    1: Icon(Icons.star, size: 20),
+                                    2: Icon(Icons.people, size: 20)
+                                  },
+                                  onValueChanged: (int? newValue) {
+                                    setState(() {
+                                      _selectedTabContent = newValue;
+                                    });
+                                  },
+                                  groupValue: _selectedTabContent,
+                                ),
+                              ),
+                              Padding(
+                                  padding: EdgeInsets.fromLTRB(5, 10, 0, 0),
+                                  child: ListView.builder(
+                                    shrinkWrap: true,
+                                    itemCount: 5,
+                                    padding: EdgeInsets.all(10),
+                                    itemBuilder: (BuildContext context, index) {
+                                      return ListTile(
+                                        title: Text(
+                                          '$_selectedTabContent Person',
+                                          textScaler: TextScaler.linear(0.8),
+                                        ),
+                                        leading: CircleAvatar(
+                                          backgroundImage: AssetImage(
+                                              'assets/images/profilePicture.jpg'),
+                                          maxRadius: 10,
+                                        ),
+                                        trailing: Icon(
+                                          Icons.mail,
+                                          size: 10,
+                                        ),
+                                      );
+                                    },
+                                  ))
                             ],
                           ),
                         )),
